f56102677c203d6f52f89c1826bfc0a91b59a844
Мереке Базарбаев <mereke.bazarbaev842@gmail.com>
2025-02-12 22:02:38 +0500

    Add files via upload

---
+++ labka#4/labarotory-work4/json.py/m1.py
@@ -0,0 +1,20 @@
+import json
+
+file_path = "C:/Users/merek/Downloads/sample-data.json"
+
+
+with open(file_path, "r") as f:
+    data = json.load(f)
+
+
+print("Inherit status")
+print("=" * 50)
+print("DN")
+print("-" * 50)
+for item in data["imdata"]:
+    attr = item["l1PhysIf"]["attributes"]
+    dn = attr.get("dn")
+    descr = attr.get("descr", "")
+    speed = attr.get("speed", "")
+    mtu = attr.get("mtu", "")
+    print(f"{dn:50} {descr:11} {speed:7} {mtu}")
